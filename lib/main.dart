56ad079aa62d81010e63658861c896a141087812
Jorik-Squirtanov228 <aleremia2003@gmail.com>
2025-10-15 15:49:42 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_3/Theme/app_colors.dart';
+import 'package:flutter_3/widgets/auth/auth_widget.dart';
+import 'package:flutter_3/widgets/main_screen/main_screen_widget.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+       appBarTheme: AppBarTheme(backgroundColor: AppColors.mainDarkBlue),
+       bottomNavigationBarTheme: BottomNavigationBarThemeData(
+           backgroundColor: AppColors.mainDarkBlue,
+          selectedItemColor: Colors.white,
+         unselectedItemColor: Colors.grey,
+       )
+      ),
+      routes: {
+        '/auth': (context) => AuthWidget(),
+        '/main_screen': (context) => MainScreenWidget(),
+      },
+      initialRoute: '/auth',
+    );
+  }
+}
+
+
